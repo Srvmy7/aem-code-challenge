2b5bfc18aa1738a05de1fbf157314e3456a2c7f2
Srvmy7 <130570678+Srvmy7@users.noreply.github.com>
2023-04-13 22:36:40 -0400

    feature/exercise-1
    feature/exercise-1

---
+++ core/src/main/java/com/anf/core/services/UserServiceConfig.java
@@ -0,0 +1,13 @@
+package com.anf.core.services;
+
+import java.util.List;
+
+public interface UserServiceConfig {
+    public String getCountryCode();
+    public String getJsonName();
+    public String getJsonPath();
+    public String getNodeName();
+    public String getNodePath();
+    public UserServiceConfig getCountryConfig(String countryCode);
+    public List<UserServiceConfig> getAllConfigs();
+}
